3e2766e540a8c223dd8d90ab86f9dd262a7699ab
Patrick McCombs <pa.mccombs1@gmail.com>
2018-03-19 19:46:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(x)
- if ((x % 3 && x % 5) == 0)
+ if x % 3 && x % 5 == 0
     "FizzBuzz"
-  elsif (x % 3) == 0
+  elsif x % 3 == 0
     "Fizz"
-  elsif (x % 5) == 0 
+  elsif x % 5 == 0 
     "Buzz"
   else
     nil
